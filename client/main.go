72ab6c80831cbf1805fecb5619009135497e668c
Carey McLelland <mclellac@ninsei.media.in.cbcsrc.ca>
2016-09-15 15:33:02 -0400

    updates

--- client/main.go
+++ client/main.go
@@ -1,82 +1,83 @@
 package main
 
 import (
-	"io"
+    "io"
 
-	"github.com/mattn/sc"
-	pb "github.com/mclellac/ok/protos/post"
+    ui "github.com/mclellac/amity/lib/ui"
+    pb "github.com/mclellac/ok/protos/post"
 
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/grpclog"
+    "github.com/mattn/sc"
+    "golang.org/x/net/context"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/grpclog"
 )
 
 const (
-	address    = "localhost:50051"
-	defaultNum = 0
+    address    = "localhost:50051"
+    defaultNum = 0
 )
 
 //const address = "127.0.0.1:11111"
 
 func add(client pb.PostServiceClient, title string, article string) error {
-	post := &pb.Post{
-		Title:   title,
-		Article: article,
-	}
-	_, err := client.AddPost(context.Background(), post)
-	return err
+    post := &pb.Post{
+        Title:   title,
+        Article: article,
+    }
+    _, err := client.AddPost(context.Background(), post)
+    return err
 }
 
 func list(client pb.PostServiceClient) error {
-	stream, err := client.ListPost(context.Background(), new(pb.RequestType))
-	if err != nil {
-		return err
-	}
-	for {
-		post, err := stream.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-		grpclog.Println(post)
-	}
-	return nil
+    stream, err := client.ListPost(context.Background(), new(pb.RequestType))
+    if err != nil {
+        return err
+    }
+    for {
+        post, err := stream.Recv()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            return err
+        }
+        grpclog.Printf("%+v %+v %+v", ui.Cyan, post, ui.Reset)
+    }
+    return nil
 }
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		grpclog.Fatalf("failed to dial: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewPostServiceClient(conn)
+    conn, err := grpc.Dial(address, grpc.WithInsecure())
+    if err != nil {
+        grpclog.Fatalf("failed to dial: %v", err)
+    }
+    defer conn.Close()
+    client := pb.NewPostServiceClient(conn)
 
-	(&sc.Cmds{
-		{
-			Name: "ls",
-			Desc: "ls: list posts",
-			Run: func(c *sc.C, args []string) error {
-				return list(client)
-			},
-		},
-		{
-			Name: "add",
-			Desc: "add \"title\" \"article\": add post",
-			Run: func(c *sc.C, args []string) error {
-				if len(args) != 2 {
-					return sc.UsageError
-				}
-				title := args[0]
-				article := args[1]
-				if err != nil {
-					return err
-				}
-				return add(client, title, article)
-			},
-		},
-	}).Run(&sc.C{
-		Desc: "The Overkill client",
-	})
+    (&sc.Cmds{
+        {
+            Name: "ls",
+            Desc: "ls: list posts",
+            Run: func(c *sc.C, args []string) error {
+                return list(client)
+            },
+        },
+        {
+            Name: "add",
+            Desc: "add \"title\" \"article\": add post",
+            Run: func(c *sc.C, args []string) error {
+                if len(args) != 2 {
+                    return sc.UsageError
+                }
+                title := args[0]
+                article := args[1]
+                if err != nil {
+                    return err
+                }
+                return add(client, title, article)
+            },
+        },
+    }).Run(&sc.C{
+        Desc: "The Overkill client",
+    })
 }
